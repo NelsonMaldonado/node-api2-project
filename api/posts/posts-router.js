434716ba745cc77251aaff4be581335cc8c71bb2
Nelson Maldonado <play2nino@gmail.com>
2022-01-17 01:36:47 -0800

    get / endpoint working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,8 +1,17 @@
 // implement your posts router here
-const express = require('express')
+const express = require("express")
 const router = express.Router()
+const Post = require("./posts-model")
 
+router.get("/", async (req, res) => {
+  try {
+    const data = await Post.find()
+    res.status(200).json(data)
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "The posts information could not be retrieved" })
+  }
+})
 
-
-
-module.exports =  router
+module.exports = router
