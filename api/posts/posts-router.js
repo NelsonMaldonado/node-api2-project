c199be0cc386040153dc9cbb2450152906eb9f0b
Nelson Maldonado <play2nino@gmail.com>
2022-01-17 01:45:45 -0800

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -14,4 +14,22 @@ router.get("/", async (req, res) => {
   }
 })
 
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" })
+      }
+      res.json(user)
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "The post information could not be retrieved",
+        error: err.message,
+      })
+    })
+})
+
 module.exports = router
